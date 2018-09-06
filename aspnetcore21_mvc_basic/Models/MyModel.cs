684e3f9d9bb2eba0c4980488210f5e94034e26ac
Michell Cronberg <admin@TA-826-INS>
2018-09-06 17:49:20 +0200

    Init

---
+++ aspnetcore21_mvc_basic/Models/MyModel.cs
@@ -0,0 +1,9 @@
+﻿// When you have others classes in /Models you can delete this file
+// (there is a @using aspnetcore21_mvc_basic.Models in _ViewImports.cshtml
+
+namespace aspnetcore21_mvc_basic.Models
+{
+    public class MyModel
+    {
+    }
+}
---
+++ aspnetcore21_mvc_basic/wwwroot/Script/app.js
@@ -0,0 +1,13 @@
+﻿(function () {    
+
+    $().ready(function () {
+        
+        setInterval(function () {
+            clk();
+        }, 1000);
+
+        function clk() {
+            $("#time").html(new Date().toLocaleTimeString("da-DK"));
+        }
+    });
+})()
