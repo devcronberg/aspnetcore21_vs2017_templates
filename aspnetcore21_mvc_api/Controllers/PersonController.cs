4722940d4ff2e39e2f12323a024c0ee4a22f02b8
Michell Cronberg <michell@cronberg.dk>
2018-09-16 11:40:29 +0200

    api

---
+++ aspnetcore21_mvc_api/Controllers/PersonController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using aspnetcore21_mvc_api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcore21_mvc_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        [Route("person")]
+        public ActionResult<List<Person>> FindPerson()
+        {
+            return new List<Person>() { new Person() { Alder = 10, Navn = "a" } };
+        }
+
+        [HttpGet]
+        [Route("person/{id}")]
+        public ActionResult<Person> FindPersoner(int id)
+        {
+            return new Person() { Alder = 10, Navn = "a" };
+        }
+
+        public ActionResult Patch(int id, [FromBody] JsonPatchDocument<Person> patch)
+        {
+            try
+            {
+                var p = new Person() { Alder = 10, Navn = "a" };
+                patch.ApplyTo(p);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
---
+++ aspnetcore21_mvc_api/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcore21_mvc_api.Models
+{
+    public class Person
+    {
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+    }
+}
